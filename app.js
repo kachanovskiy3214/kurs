552cdc70a0a0c6cce82f054b8dfcc90ae338e954
kachanovskiy3214 <140253454+kachanovskiy3214@users.noreply.github.com>
2024-05-06 12:04:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 80;
 const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
